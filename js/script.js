b21f2bb5d962df64eae728b7d8bb7c5adb3aa8f2
3paK <KuTaMuH@Gmail.com>
2023-04-11 16:27:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+    // "use strict";
+
+    const category = `toys`;
+
+    console.log(`https://someurl.com/${category}/5/yo`);
+
+
+    const user = `Ivan`;
+
+    alert(`Привет, ${user}`);
+    
