e8e244f3c13985fad3440840684685ad33a7091c
3paK <KuTaMuH@Gmail.com>
2023-07-03 21:48:13 +0300

    IF_ELSE

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
     // "use strict";
 
-    const nomberOfFilms = +prompt(`сколько фильмов вы уже просмотрели?`, ``);
-
-    const personalMoveDB = {
-        count: nomberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
-    const a = prompt(`Один из последних просмотренных фильмов?`, ``),
-          b = prompt(`На сколько оцените его?`, ``),
-          c = prompt(`Один из последних просмотренных фильмов?`, ``),
-          d = prompt(`На сколько оцените его?`, ``);
-
-    personalMoveDB.movies[a] = b;
-    personalMoveDB.movies[c] = d;
     
-    console.log(personalMoveDB);
-    
+
+   // (num === 50) ? console.log(`ok!`) : console.log(`no`);
+
+   const  num = 49;
+
+   switch(num) {
+        case 49:
+            console.log(`no`);
+            break;
+        case 51:
+            console.log(`yo!`);
+            break;
+        case 99:
+            console.log(`MHO`);
+            break;
+        default:
+            console.log(`NICHT VOBLA !!!!!`);
+            break;     
+   }
