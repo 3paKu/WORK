79d57e76ac03226036e883de686d9799dc322a53
3paK <KuTaMuH@Gmail.com>
2024-01-06 01:42:10 +0300

    TIMER+END_TIMER

--- FOOD/js/script.js
+++ FOOD/js/script.js
@@ -42,18 +42,27 @@ window.addEventListener(`DOMContentLoaded`, () => {
         const deadline = `2024-05-11`;
 
         function getTimeRemaining(endtime) {
-            const t = Date.parse(endtime) - Date.parse(new Date()),
-                days = Math.floor(t / (1000 * 60 * 60 * 24)),
-                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-                minutes = Math.floor((t / 1000 * 60) % 60),
-                seconds = Math.floor((t / 1000) % 60);
+            let days,hours,minutes,seconds;
+            const t = Date.parse(endtime) - Date.parse(new Date());
+                
+                if (t <= 0) {
+                    days = 0;
+                    hours = 0;
+                    minutes = 0;
+                    seconds = 0;
+                } else {
+                    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                    hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+                    minutes = Math.floor((t / 1000 * 60) % 60),
+                    seconds = Math.floor((t / 1000) % 60);
+                }
 
-            return {
-                'total': t,
-                'days': days,
-                'hours': hours,
-                'minutes': minutes,
-                'seconds': seconds
+                return {
+                    'total': t,
+                    'days': days,
+                    'hours': hours,
+                    'minutes': minutes,
+                    'seconds': seconds
             };
         }
 
@@ -65,8 +74,8 @@ window.addEventListener(`DOMContentLoaded`, () => {
             }
         }
 
-        function setClock(selector, endtime) {
-            const timer = document.querySelector(selector),
+        function setClock(selector, endtime) {           
+            const timer = document.querySelector(selector),        
                 days = timer.querySelector('#days'),
                 hours = timer.querySelector('#hours'),
                 minutes = timer.querySelector('#minutes'),
