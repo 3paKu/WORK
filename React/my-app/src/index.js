2494742a97661d15a0231d7680bc060c38af4ac2
3paK <KuTaMuH@Gmail.com>
2024-03-02 15:43:43 +0300

    BASIC_JSX

---
+++ React/my-app/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+  const text ='Hello World';
+
+  const elem = (
+    <div>
+      <h2 className="text"> Текст : {text}</h2>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <button tabIndex={0}/>
+    </div>
+  );
+
+  // const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  elem
+);
+
+
